8373d6ca35f42f0ddd492af401a921baedf9a688
blast66 <113650618+blast66@users.noreply.github.com>
2022-09-30 11:44:31 +0800

    Add files via upload

---
+++ Scanner練習.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class test {
+    public static void main(String []args) {
+		int num1;
+		int num2;
+		int sum;
+		
+		Scanner intput = new Scanner(System.in);
+		System.out.println("ENTER YOU NUM1");
+		num1 = intput.nextInt();
+		
+		System.out.println("ENTER YOU NUM1");
+		num2 = intput.nextInt();
+		sum = num1+num2
+        if(sum >= 10){
+			System.out.println("bitch");
+			
+		}
+		else{
+			System.out.println("big gg");
+			
+		}
+	}
+	
+
+
+}
---
+++ ifb.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class ifa {
+    public static void main(String []args) {
+		int num1;
+		int num2;
+		int sum;
+		
+		Scanner intput = new Scanner(System.in);
+		System.out.println("ENTER YOU NUM1");
+		num1 = intput.nextInt();
+		
+		System.out.println("ENTER YOU NUM1");
+		num2 = intput.nextInt();
+		sum = num1+num2;
+		
+        if(sum >= 50)
+			if(sum <= 70)
+			  System.out.println("bitch");
+			else
+				System.out.println("big gg");
+			
+		
+	}
+	
+
+
+}
